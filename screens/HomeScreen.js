26fbeeea1ec7b37f04c8ffb5ff70d083d1ae7f87
Poker Lamp <93736740+JoseLohan@users.noreply.github.com>
2022-10-04 16:38:56 -0300

    Projeto 63

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,75 +3,65 @@ import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-nativ
 import { Header } from 'react-native-elements';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-export default class HomeScreen extends Component{
+export default class HomeScreen extends Component {
   constructor() {
     super();
     this.state = {
       text: '',
       isSearchPressed: false,
       isLoading: false,
-      word  : "Carregando...",
-      lexicalCategory :'',
-      definition : ""
+      word: "Carregando...",
+      lexicalCategory: '',
+      definition: ""
     };
   }
 
-  getWord=(word)=>{
-    var searchKeyword=word.toLowerCase()
+  getWord = (word) => {
+    var searchKeyword = word.toLowerCase()
     var url;
-    //url = "https://rupinwhitehatjr.github.io/dictionary/searchKeyword.json"
-    //url = "https://rupinwhitehatjr.github.io/dictionary/"+ +".json"
-    //url = "https://rupinwhitehatjr.github.io/dictionary/"+searchKeyword+".json"
-    //url = "https://rupinwhitehatjr.github.io/dictionary/"+word+".json"
-
-    //API traduzida (contém 5 palavras: aplicativo, aprendizado, escola, linguagem e professora)
-    //url = "https://thiagosaraiva-git.github.io/dictionary-master/"+word+".json"
+    url = "https://rupinwhitehatjr.github.io/dictionary/"+searchKeyword+".json"
     
     return fetch(url)
-    .then((data)=>{
-      if(data.status===200)
-      {
-        return data.json()
-      }
-      else
-      {
-        return null
-      }
-    })
-    .then((response)=>{
+      .then((data) => {
+        if (data.status === 200) {
+          return data.json()
+        }
+        else {
+          return null
+        }
+      })
+      .then((response) => {
 
         var responseObject = response
-        if(responseObject)
-        {
+        if (responseObject) {
           var wordData = responseObject.definitions[0]
 
-          var definition=wordData.description
-          var lexicalCategory=wordData.wordtype
+          var definition = wordData.description
+          var lexicalCategory = wordData.wordtype
 
           this.setState({
-            "word" : this.state.text, 
-            "definition" :definition,
-            "lexicalCategory": lexicalCategory     
-            
+            "word": this.state.text,
+            "definition": definition,
+            "lexicalCategory": lexicalCategory
+
           })
         }
-        else
-        {
+        else {
           this.setState({
-            "word" : this.state.text, 
-            "definition" :"Não encontrado",
-            
+            "word": this.state.text,
+            "definition": "Não encontrado",
+
           })
 
         }
-    
-    })
+
+      })
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <SafeAreaProvider>
-          <View style={{flex:1, borderWidth:2}}>
+        <View style={{ flex: 1, borderWidth: 2 }}>
           <Header
             backgroundColor={'purple'}
             centerComponent={{
@@ -80,62 +70,20 @@ export default class HomeScreen extends Component{
             }}
           />
           <View style={styles.inputBoxContainer}>
-        
-          <Text 
-            onChangeText={text => {
-                this.setState({
-                  text: text,
-                  isSearchPressed: false,
-                  word  : "Carregando...",
-                  lexicalCategory :'',
-                  examples : [],
-                  definition : ""
-                });
-            }}
-          />
-        {/*   <input 
-           onChangeText={text => {
-              this.setState({
-                text: text,
-                isSearchPressed: false,
-                word  : "Loading...",
-                lexicalCategory :'',
-                examples : [],
-                definition : ""
-              });
-           }}
-        /> 
-        */}
-        {/*
-        <TouchableOpacity 
-            onChangeText={text => {
-                this.setState({
-                  text: text,
-                  isSearchPressed: false,
-                  word  : "Carregando...",
-                  lexicalCategory :'',
-                  examples : [],
-                  definition : ""
-                });
-            }}
-          /> 
-        */}
-        {/*
-            <TextInput
-              style={styles.inputBox}
-              onChangeText={text => {
-                this.setState({
-                  text: text,
-                  isSearchPressed: false,
-                  word  : "Carregando...",
-                  lexicalCategory :'',
-                  examples : [],
-                  definition : ""
-                });
-              }}
-              value={this.state.text}
-            />
-            */}
+              <TextInput
+                style={styles.inputBox}
+                onChangeText={text => {
+                  this.setState({
+                    text: text,
+                    isSearchPressed: false,
+                    word: "Carregando...",
+                    lexicalCategory: '',
+                    examples: [],
+                    definition: ""
+                  });
+                }}
+                value={this.state.text}
+              />
             <TouchableOpacity
               style={styles.searchButton}
               onPress={() => {
@@ -146,22 +94,22 @@ export default class HomeScreen extends Component{
             </TouchableOpacity>
           </View>
           <View style={styles.outputContainer}>
-            <Text style={{fontSize:20}}>
+            <Text style={{ fontSize: 20 }}>
               {
                 this.state.isSearchPressed && this.state.word === "Carregando..."
-                ? this.state.word
-                : ""
+                  ? this.state.word
+                  : ""
               }
             </Text>
-              {
-                this.state.word !== "Carregando..." ?
+            {
+              this.state.word !== "Carregando..." ?
                 (
-                  <View style={{justifyContent:'center', marginLeft:10 }}>
+                  <View style={{ justifyContent: 'center', marginLeft: 10 }}>
                     <View style={styles.detailsContainer}>
                       <Text style={styles.detailsTitle}>
                         Palavra :{" "}
                       </Text>
-                      <Text style={{fontSize:18 }}>
+                      <Text style={{ fontSize: 18 }}>
                         {this.state.word}
                       </Text>
                     </View>
@@ -169,22 +117,22 @@ export default class HomeScreen extends Component{
                       <Text style={styles.detailsTitle}>
                         Tipo :{" "}
                       </Text>
-                      <Text style={{fontSize:18}}>
+                      <Text style={{ fontSize: 18 }}>
                         {this.state.lexicalCategory}
                       </Text>
                     </View>
-                    <View style={{flexDirection:'row',flexWrap: 'wrap'}}>
+                    <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
                       <Text style={styles.detailsTitle}>
                         Definição :{" "}
                       </Text>
-                      <Text style={{ fontSize:18}}>
+                      <Text style={{ fontSize: 18 }}>
                         {this.state.definition}
                       </Text>
                     </View>
                   </View>
                 )
-                :null
-              }
+                : null
+            }
           </View>
         </View>
       </SafeAreaProvider>
@@ -194,9 +142,9 @@ export default class HomeScreen extends Component{
 
 const styles = StyleSheet.create({
   inputBoxContainer: {
-    flex:0.3,
-    alignItems:'center',
-    justifyContent:'center'
+    flex: 0.3,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   inputBox: {
     width: '80%',
@@ -214,21 +162,21 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderRadius: 10,
   },
-  searchText:{
+  searchText: {
     fontSize: 20,
     fontWeight: 'bold'
   },
-  outputContainer:{
-    flex:0.7,
-    alignItems:'center'
+  outputContainer: {
+    flex: 0.7,
+    alignItems: 'center'
   },
-  detailsContainer:{
-    flexDirection:'row',
-    alignItems:'center'
+  detailsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center'
   },
-  detailsTitle:{
-    color:'orange',
-    fontSize:20,
-    fontWeight:'bold'
+  detailsTitle: {
+    color: 'orange',
+    fontSize: 20,
+    fontWeight: 'bold'
   }
 });
